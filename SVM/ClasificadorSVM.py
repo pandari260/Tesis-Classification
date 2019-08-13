63f1f35da7d32966cb2366446ff9820de9cffe97
pandari260 <pandari260@gmail.com>
2019-08-13 18:14:15 -0300

    modificacion del clasificador

--- SVM/ClasificadorSVM.py
+++ SVM/ClasificadorSVM.py
@@ -5,23 +5,30 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 
+class ClasificadorSVM:
 
-bankdata = pd.read_csv("input/input.csv")
+    def clasificarMuestra(self):
+    
+        bankdata = pd.read_csv("input/input.csv")
 
-bankdata.shape
-bankdata.head()
+        bankdata.shape
+        bankdata.head()
+        X = bankdata.drop('Class', axis=1)
+        y = bankdata['Class']
 
-X = bankdata.drop('Class', axis=1)
-y = bankdata['Class']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.7)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.7)
+        #svclassifier = SVC(kernel='rbf')
+       
+        svclassifier = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+        decision_function_shape='ovr', degree=3, gamma='scale', kernel='rbf',
+        max_iter=-1, probability=False, random_state=None, shrinking=True,
+        tol=0.001, verbose=False)
+        svclassifier.fit(X_train, y_train)
 
-svclassifier = SVC(kernel='rbf')
-svclassifier.fit(X_train, y_train)
+        print(svclassifier.score(X_test, y_test))
+        y_pred = svclassifier.predict(X_test)
 
-print(svclassifier.score(X_test, y_test))
-y_pred = svclassifier.predict(X_test)
-
-print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred))
+        print(confusion_matrix(y_test, y_pred))
+        print(classification_report(y_test, y_pred))
 
--- SVM/Main.py
+++ SVM/Main.py
@@ -2,26 +2,29 @@ import GraficadorMuestral as graficador
 import GeneradorMuestral as generador
 import ConversorMuestral as conversor
 import ExportadorArchivo as exportador
+import ClasificadorSVM as clasificador
 import numpy as np
 
 gen = generador.GeneradorMuestral()
 graf = graficador.GraficadorMuestral()
 conver = conversor.ConversorMuestral()
 exp = exportador.ExportadorArchivo()
+clas = clasificador.ClasificadorSVM()
 
-muestraClase1 = gen.generarMuestraDN(0,1,2,10)
+muestraClase1 = gen.generarMuestraDN(0,1,3,100)
 muestrasGeneradasC1 = conver.convertirMuestra(muestraClase1,1)
 
-muestraClase2 = gen.generarMuestraDN(3,1,2,10)
+muestraClase2 = gen.generarMuestraDN(2,1,3,100)
 muestrasGeneradasC2 = conver.convertirMuestra(muestraClase2,0)
 
-print("Clase a la que pertenece: "+str(conver.getClase((1.2492,2.5049))))
+#print("Clase a la que pertenece: "+str(conver.getClase((1.4586,4.6703,2.5005))))
 
 #print(muestrasGeneradasC1+muestrasGeneradasC2)
 #x,y = gen.generarNdimensional(0,100,2,100)
 #graf.graficarBidimensional(x, y)
 #graf.graficarBidimensional(xx,yy)
 exp.exportarArchivoCSV(muestrasGeneradasC1+muestrasGeneradasC2)
+clas.clasificarMuestra()
 
 
 
