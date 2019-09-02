4c41d16e17ac70588141d4a62cf8298bb7c50d98
Ariel <arieldominguezmp@gmail.com>
2019-09-02 01:30:51 -0300

    Cambios minimos

--- SVM/Exporter.py
+++ SVM/Exporter.py
@@ -7,16 +7,16 @@ def exportSampleSVM(sample, location):
         setFormat(data, file)
     file.close()
 
+def exportSampleZPL(sample, location):
+    file = open(location, "w")
+    setFormat(sample, file)
+    file.close()
+
 def generateHeader(sample, location, target):
     file = open(location, target)
     for i in range(len(sample[0][0])-1):
         file.write("Feature"+str(i+1)+",")
     file.write('Class\n')
-    file.close()
-
-def exportSampleZPL(sample, location):
-    file = open(location, "w")
-    setFormat(sample, file)
     file.close()
 
 def setFormat(sample, file):
--- SVM/Main.py
+++ SVM/Main.py
@@ -30,19 +30,20 @@ coves2 = [   [[1,0],[0,1]],
             [[1,0],[0,1]],
             [[1,0],[0,1]] ]
 
-n2 = [100,100,100]
+n2 = [2,2,3]
 clases2 = [1,0,1]
 
 # Se espera 0.997272
-#print( coves)
+# print( coves)
 
 sample = sample.generateSample(mues2, coves2, n2, clases2)
 c0,c1 = transform.transformSampleZPL(sample)
-exporter.exportSampleZPL(c0,'input/zplC0.csv')
-exporter.exportSampleZPL(c1,'input/zplC1.csv')
-exporter.exportSampleSVM(sample,'input/input.csv')
-resultClassify, dataTest = svm.classify('rbf', 'input/input.csv')
-printer.printOutputSVM(resultClassify, dataTest)
+print(sample)
+#exporter.exportSampleZPL(c0,'input/zplC0.csv')
+#exporter.exportSampleZPL(c1,'input/zplC1.csv')
+#exporter.exportSampleSVM(sample,'input/input.csv')
+#resultClassify, dataTest = svm.classify('rbf', 'input/input.csv')
+#printer.printOutputSVM(resultClassify, dataTest)
 
 
 
