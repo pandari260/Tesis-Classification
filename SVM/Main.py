b93a46958323e7abe4a7b878418a9e27b17e7c8d
Ariel <arieldominguezmp@gmail.com>
2019-10-08 22:25:28 -0300

    algo que falto

--- SVM/Main.py
+++ SVM/Main.py
@@ -39,18 +39,14 @@ clases2 = [1,0,1]
 sample = sample.generateSample(mues2, coves2, n2, clases2)
 #c0,c1 = transform.transformSampleZPL(sample)
 
-sampleC0 = [(1,1),(2,2),(1.3,1.5),(1.7,1.8)]
-sampleC1 = [(3,3),(5,5),(5.3,5.5),(6.7,6.8)]
-setHiperplan = [(2,2,-1)]
-printer.printSampleR2(sampleC0, sampleC1, setHiperplan)
 
 
 #exporter.exportSampleZPL(c0,'%s,%s,%s \n', 'input/zplC0.csv')
 #exporter.exportSampleZPL(c1,'%s,%s,%s \n', 'input/zplC1.csv')
 #print(c0)
 #exporter.exportSampleSVM(sample,'%s,%s,%s \n','input/input.csv')
-#resultClassify, dataTest = svm.classify('rbf', 'input/input.csv')
-#printer.printOutputSVM(resultClassify, dataTest)
+resultClassify, dataTest = svm.classify('rbf', 'input/input.csv')
+printer.printOutputSVM(resultClassify, dataTest)
 
 
 
