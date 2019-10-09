643b7648eab4385672165ecee0d0a9d9da1e2f8a
Ariel <arieldominguezmp@gmail.com>
2019-10-08 22:23:41 -0300

    Se implemento el Plotter
    Es un graficador de hyperplanos y conjunto de muestras  de dos clases en R2

---
+++ CRIO/Plotter.py
@@ -0,0 +1,55 @@
+import numpy as np  
+import matplotlib.pyplot as plt  
+
+def f(coef, scale):
+    return (coef[2] - (coef[0]*scale))  / coef[1]
+
+def drawHyperPlane(coef, scale):
+	c = 'orange'
+	if(coef[0] != 0 and coef[1]!=0):
+		plt.plot(scale, f(coef,scale))
+		plt.fill_between(scale, f(coef,scale), scale[0], color=c, alpha=0.3)
+	else:
+		if(coef[0] == 0):
+			y = (coef[2] / coef[1])
+			plt.axhline(y)
+			plt.axhspan(y, scale[0], alpha=0.3, color=c)
+		else: 
+			x = (coef[2] / coef[0])
+			plt.axvline(x)
+			plt.axvspan(x, scale[0], alpha=0.3, color=c)
+	
+	
+def graphHyperplane(setPlane, scale):
+	for i in setPlane:
+		drawHyperPlane(i, scale)
+
+def graphSample(sampleC0, sampleC1):      
+    drawSample(sampleC0, 'red')
+    drawSample(sampleC1, 'blue')
+
+def drawSample(sample, color):
+    x, y = [], []
+    area = np.pi*10
+    for i in sample:
+        x.append(i[0])
+        y.append(i[1])
+    plt.scatter(x, y, s=area, c=color, alpha=1)
+
+def graphDataSet(sampleC0, sampleC1, setPlane, scale):
+	graphSample(sampleC0, sampleC1)
+	graphHyperplane(setPlane, scale)
+	plt.show()
+
+sampleC0 = [(1,1),(2,2),(1.3,1.5),(1.7,1.8)]
+sampleC1 = [(3,3),(5,5),(5.3,5.5),(6.7,6.8)]
+setPlane = [(2,3,10),(0,3,10),(2,0,10)]
+
+#graphSample(sampleC0, sampleC1)
+n = np.linspace(-100,100, 100)
+#graphHyperplane(setPlane, n)
+graphDataSet(sampleC0, sampleC1, setPlane, n)
+
+
+#plt.show()
+
--- SVM/Printer.py
+++ SVM/Printer.py
@@ -2,30 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, recall_score
 
-def printSampleR2(sampleC0, sampleC1, setHiperplan):      
-    loadHiperplan(setHiperplan)
-    loadSample(sampleC0, 'red')
-    loadSample(sampleC1, 'blue')
-    plt.show()
-
-def loadHiperplan(setHiperplan):
-    X  = np.linspace(-15,15, 10)
-
-    for recta in setHiperplan:
-        plt.plot(X, f(recta, X), color='orange')
-        plt.fill_between(X, f(recta,X),-100,color='orange',alpha=.5)
-
-def loadSample(sample, color):
-    x, y = [], []
-    area = np.pi*10
-    for i in sample:
-        x.append(i[0])
-        y.append(i[1])
-    plt.scatter(x, y, s=area, c=color, alpha=0.5)
-
-def f(recta,x):
-    return recta[0]*x + recta[1]*x + recta[2] 
-
 def printOutputSVM(Y_test, y_pred):   
     print("Accuracy: ", accuracy_score(Y_test, y_pred))
     print("Confusion matrix: ")
