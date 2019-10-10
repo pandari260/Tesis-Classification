70b660928c05fb0570b8b4962050a5ef604a19d2
Ariel <arieldominguezmp@gmail.com>
2019-10-09 22:14:01 -0300

    Pruebas con el Plotter

--- CRIO/Plotter.py
+++ CRIO/Plotter.py
@@ -5,19 +5,19 @@ def f(coef, scale):
     return (coef[2] - (coef[0]*scale))  / coef[1]
 
 def drawHyperPlane(coef, scale):
-	c = 'orange'
+	cFill, cLine = 'orange', 'black'
 	if(coef[0] != 0 and coef[1]!=0):
-		plt.plot(scale, f(coef,scale))
-		plt.fill_between(scale, f(coef,scale), scale[0], color=c, alpha=0.3)
+		plt.plot(scale, f(coef,scale), color=cLine)
+		plt.fill_between(scale, f(coef,scale), scale[0], color=cFill, alpha=0.2)
 	else:
 		if(coef[0] == 0):
 			y = (coef[2] / coef[1])
-			plt.axhline(y)
-			plt.axhspan(y, scale[0], alpha=0.3, color=c)
+			plt.axhline(y, color=cLine)
+			plt.axhspan(y, scale[0], alpha=0.2, color=cFill)
 		else: 
 			x = (coef[2] / coef[0])
-			plt.axvline(x)
-			plt.axvspan(x, scale[0], alpha=0.3, color=c)
+			plt.axvline(x, color=cLine)
+			plt.axvspan(x, scale[0], alpha=0.2, color=cFill)
 	
 	
 def graphHyperplane(setPlane, scale):
@@ -41,12 +41,12 @@ def graphDataSet(sampleC0, sampleC1, setPlane, scale):
 	graphHyperplane(setPlane, scale)
 	plt.show()
 
-#sampleC0 = [(1,1),(2,2),(1.3,1.5),(1.7,1.8)]
-#sampleC1 = [(3,3),(5,5),(5.3,5.5),(6.7,6.8)]
-#setPlane = [(2,3,10),(0,3,10),(2,0,10)]
+sampleC0 = [(1,1),(2,2),(1.3,1.5),(1.7,1.8)]
+sampleC1 = [(3,3),(5,5),(5.3,5.5),(6.7,6.8)]
+setPlane = [(2,3,10),(0,3,10),(2,0,10),(-3,5,30),(-20,4,10)]
 
-#n = np.linspace(-100,100, 100)
-#graphDataSet(sampleC0, sampleC1, setPlane, n)
+n = np.linspace(-100,100, 100)
+graphDataSet(sampleC0, sampleC1, setPlane, n)
 
 
 
