74f9588ab7ec865c31a4faa1a85c69059ae9f5cc
Ariel <arieldominguezmp@gmail.com>
2019-10-11 16:31:30 -0300

    Se agregan mas casos de test

--- TEST/MainTest.py
+++ TEST/MainTest.py
@@ -52,11 +52,10 @@ name = "t5d2-ConjuntosCruz"
 d = 2
 n = [500, 500]
 mMu = [[0,0],[0,0]]
-classes = [0,1]
+classes = [1,0]
 mCov = [[[10,10],[5000,10]], [[10,10],[1000,1000]]]
 caso5 = caseTest.CaseTest(name, d, n, mMu, mCov, classes)
 #caso5.runTest()
-
 
 name = "t6d2-CuadranteOpuesto"
 d = 2
@@ -66,3 +65,13 @@ classes = [1,0,0,1]
 mCov = []
 caso6 = caseTest.CaseTest(name, d, n, mMu, mCov, classes)
 #caso6.runTest()
+
+
+name = "t7d2-DiagonalIntercalada"
+d = 2
+n = [500, 500, 500]
+mMu = [[2,10],[8,8],[10,6]]
+classes = [1,0,1]
+mCov = []
+caso7 = caseTest.CaseTest(name, d, n, mMu, mCov, classes)
+caso7.runTest()
