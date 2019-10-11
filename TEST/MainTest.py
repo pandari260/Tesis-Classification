f0494c66cbea71ced155c7ff4e3d401f4ac9d311
Ariel <arieldominguezmp@gmail.com>
2019-10-11 18:30:13 -0300

    Nuevos casos de prueba

--- TEST/MainTest.py
+++ TEST/MainTest.py
@@ -77,8 +77,29 @@ caso7 = caseTest.CaseTest(name, d, n, mMu, mCov, classes)
 #caso7.runTest()
 
 
+name = "t8d2-DiagonalIntercaladaSolapada"
+d = 2
+n = [500, 500, 500]
+mMu = [[2,10],[5,8],[8,6]]
+classes = [1,0,1]
+mCov = []
+caso8 = caseTest.CaseTest(name, d, n, mMu, mCov, classes)
+#caso8.runTest()
+
+
+name = "t9d2-Encerrado"
+d = 2
+n = [500, 500, 500, 500, 500, 500, 500, 500, 500]
+mMu = [[0,0],[0,10],[5,5],[5,0],[10,0],[10,10],[5,10], [0,5], [10,5]]
+classes = [1,1,0,1,1,1,1,1,1]
+mCov = []
+caso9 = caseTest.CaseTest(name, d, n, mMu, mCov, classes)
+caso9.runTest()
+
+
+"""
 c0 = [(4.0, 5.0), (4.0, 3.0), (5.0, 4.0)]
 c1 = [(4.0, 4.0), (6.0, 4.0)]
 h = [(0.0, -1.99999991038, -6.99999968634),(1.9999999, 1.9999999, 16.99999915),(-0.799999960993,-0.399999980101,-5.39999973532),(-1.99999990568, 0.00043, -10.9999994659)]
 n = np.linspace(-10,10,3000)
-plotter.graphDataSet(c0,c1,h,n)
+plotter.graphDataSet(c0,c1,h,n)"""
