63a8c85038ec3912bc0c015c1d8598f4c6895fd8
Javier <xavierescobar3335@gmail.com>
2019-10-11 16:59:33 -0300

    AHora las muestras se leen como float

--- pythonProject/CRIO/Importer.py
+++ pythonProject/CRIO/Importer.py
@@ -1,7 +1,7 @@
-def listStringToInteger(list):
+def listStringToFloat(list):
     l = list[:]
     for j in range(0, len(l)):
-        l[j] = (int(l[j]))
+        l[j] = (float(l[j]))
     return l
 
 #lee el archivo de muestras en formato zpl indicados en la ruta y retorna un lista de muestras en formato de tuplas.  
@@ -11,7 +11,7 @@ def readSamples(route, dimension):
     
     lines = list(map(lambda line: line[:-1], lines))
     lines = list(map(lambda line: line.split(","), lines))
-    data = list(map(lambda line: listStringToInteger(line), lines))        
+    data = list(map(lambda line: listStringToFloat(line), lines))        
     print("data: " + str(data))
     clase = []
     sample = []
