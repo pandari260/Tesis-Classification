c17e6cafcd5490a1bc3f178d2cfcfbfe1779d9b5
Javier <xavierescobar3335@gmail.com>
2019-10-11 13:34:15 -0300

    Se creo el proyecto CRIO en eclipse

---
+++ pythonProject/CRIO/crioClassifier.py
@@ -0,0 +1,90 @@
+from clusteringV2 import ClusterContainer 
+import grouper as Grouper
+from fileManager import writeClusters,writeParameters, readSamples
+import hiperplaneDefiner as Hiperplane
+from astroid.__pkginfo__ import classifiers
+from __builtin__ import False
+import functools
+
+#TO DO: parametrizar estos parametros
+#routeClase0 = "model/class0.dat"
+#routeClase1 = "model/class1.dat"
+routeCluster0 = "model/cluster0.dat"
+routeCluster1 = "model/cluster1.dat"
+routeParameters = "model/globalParameters"
+#dimenssion = 2
+#k = 2
+M = 1000000
+
+
+class Classifier():
+    def __init__(self, route0,route1,k,d, t0, t1):
+        self.routeClase0 = route0
+        self.routeClase1 = route1
+        self.numGroups = k
+        self.dimenssion = d
+        self.regions = self.trainClasificator()
+        self.tag0 = t0
+        self.tag1 = t1
+        
+
+    
+
+    def trainClasificator(self):
+        
+        class0 = readSamples(self.routeClase0, self.dimenssion)
+        class1 = readSamples(self.routeClase1, self.dimenssion)        
+        
+        clusterContainer0 = ClusterContainer(class0,class1)
+        clusterContainer1 = ClusterContainer(class1,class0)        
+        
+        writeClusters(clusterContainer0, class0,routeCluster0)
+        writeClusters(clusterContainer1, class1, routeCluster1)
+    
+        parameters = [(len(class0 + class1)), self.dimenssion, len(class0), len(class1), self.numGroups, clusterContainer0.getCantClusters(), clusterContainer1.getCantClusters(),M]
+        writeParameters(parameters, routeParameters)
+    
+        groupContainer = Grouper.GroupContainer(class1, clusterContainer1, self.numGroups)
+        Grouper.deleteOutliers(class0, clusterContainer0, groupContainer.model.e0Vals)
+        
+        regions = Hiperplane.defineHiperplanes(groupContainer, clusterContainer0)
+        return regions
+    
+   
+    def classify(self, sample):
+        ret = self.tag0
+        for value in self.regions.values():
+            results = list(map(lambda hiperplane: hiperplane[0]*sample[0] + hiperplane[1]*sample[1] <= hiperplane[2], value))
+            print("resultados: " + str(results) + "\n")
+            if reduce(lambda a,b: a and b, results):
+                ret = self.tag1
+                break
+        return ret
+        
+        
+        
+    def printRegions(self):
+        print("-----------------------------------------------------------------------------------------------------------\n")
+        for key, value in self.regions.items():
+            print("Region: " + str(key) + " \n Hiperplanos:\n ")
+            print("cantidad: " + str(len(key)))
+            for hiperplano in value:
+                    print(str(hiperplano) + "\n")
+            print("-----------------------------------------------------------------------------------------------------------\n")
+
+        
+        
+        
+
+def main():
+    classifier = Classifier("model/class0.dat","model/class1.dat",2,2,"azul", "rojo")
+    print("la muestra " + str((8,4)) +  " es de color: " +  str(classifier.classify((8,4))))
+    print("la muestra " + str((2,4)) +  " es de color: " +  str(classifier.classify((2,4))))
+    print("la muestra " + str((4,8)) +  " es de color: " +  str(classifier.classify((4,8))))
+    print("la muestra " + str((4,2)) +  " es de color: " +  str(classifier.classify((4,2))))
+
+    
+    
+
+main()
+    
---
+++ pythonProject/CRIO/grouper.py
@@ -0,0 +1,74 @@
+from scipInterface import solveProblem
+
+
+routeModel = "model/assingGroups.zpl"
+
+class GroupContainer():
+    
+    def __init__(self,class1,clusterContainer,k):
+        self.model = ModelSolution(solveProblem(routeModel),clusterContainer.getCantClusters(), 3,2,k)        
+        deleteOutliers(class1, clusterContainer, self.model.e1Vals)        
+        self.groups = defineGroups(clusterContainer,self.model, k)
+        self.cantGroups = len(self.groups)     
+    
+    def getGroups(self):
+        return self.groups
+    
+    def getNumberGroups(self):
+        return self.cantGroups
+
+
+
+#crea lista de muestras con las muestras contenidas en los cluster correspondientes a cada grupo
+def defineGroups(clusterContainer, model, k):
+    
+    assings = assingGroups(clusterContainer.getCantClusters(), model, k)
+    
+    clusters = clusterContainer.getClusters()
+    groups=[]
+    for value in assings.values():
+        samples=[]
+        for i in value:
+            samples = samples + clusters[i]
+        if len(samples) > 0:
+            groups.append(tuple(samples))
+    return groups
+
+
+#assigna cada id de cluster a uno de los k grupos
+def assingGroups(cantClusters,model, k):
+    
+    groups = dict()
+    arkVals = model.arkVals  
+    
+        
+    for i in range(0, k):
+        g = []
+        for j in range(cantClusters*i, cantClusters*(i+1)):
+            if(arkVals[j] == 1):
+                g.append(j%cantClusters)
+        groups[i] = g
+
+    return groups
+
+def deleteOutliers(class1, clusterContainer, eVals):
+    
+    for e in range(0, len(eVals)):
+        if eVals[e] > 1:
+            clusterContainer.removeSample(class1[e])
+    
+    
+    return 0
+    
+    
+class ModelSolution():
+    
+    def __init__(self, model, cantClusters,m0,m1 , k):
+            self.arkVals = list(map(lambda var: model.getVal(var),model.getVars()[:cantClusters*k]))    
+            self.e0Vals = list(map(lambda var: model.getVal(var),model.getVars()[cantClusters*k:cantClusters*k+m0] ))   
+            self.e1Vals = list(map(lambda var: model.getVal(var),model.getVars()[cantClusters*k+m0:cantClusters*k+m0+ m1])) 
+        
+        
+            
+            
+
