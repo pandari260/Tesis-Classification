48d237782a58fe5b2fb30a4321a8fe29b5ed9e11
Javier <xavierescobar3335@gmail.com>
2019-10-11 14:45:06 -0300

    Se creo la clase importer

--- pythonProject/CRIO/Classifier.py
+++ pythonProject/CRIO/Classifier.py
@@ -1,6 +1,7 @@
 from Clustering import ClusterContainer 
 from CRIO import Grouper as Grouper
-from CRIO.Exporter import writeClusters,writeParameters, readSamples, writeSolution
+from Exporter import writeClusters,writeParameters, writeSolution
+from Importer import readSamples
 from CRIO import HiperplaneDefiner as Hiperplane
 from astroid.__pkginfo__ import classifiers
 from __builtin__ import False
--- pythonProject/CRIO/Exporter.py
+++ pythonProject/CRIO/Exporter.py
@@ -4,29 +4,7 @@ THREE_ITEMS_FORMAT = "%s,%s,%s\n"
 
 
 
-#toma un list de string y la transforma en una list de enteros
-def listStringToInteger(list):
-    l = list[:]
-    for j in range(0, len(l)):
-        l[j] = (int(l[j]))
-    return l
 
-#lee el archivo de muestras en formato zpl indicados en la ruta y retorna un lista de muestras en formato de tuplas.  
-def readSamples(route, dimension):
-    f = open(route,"r")
-    lines = f.readlines()
-    
-    lines = list(map(lambda line: line[:-1], lines))
-    lines = list(map(lambda line: line.split(","), lines))
-    data = list(map(lambda line: listStringToInteger(line), lines))        
-   
-    clase = []
-    for i in range(0, len(data) -1,2):
-        clase.append(tuple(list(zip(data[i],data[i+1]))[2]))
-        #clase.append(np.array((list(zip(data[i],data[i+1]))[2])))
-
-    f.close()
-    return clase
 
 def writeGroup(group, route):
     f = open(route,"w")
