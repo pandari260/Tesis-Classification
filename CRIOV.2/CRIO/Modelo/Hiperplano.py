7f684664df03ee599e97b9b87bef49f052be2a6a
Javier <xavierescobar3335@gmail.com>
2019-12-24 19:17:30 -0300

    el cuarto modelo

--- CRIOV.2/CRIO/Modelo/Hiperplano.py
+++ CRIOV.2/CRIO/Modelo/Hiperplano.py
@@ -3,6 +3,7 @@ Created on 19 dic. 2019
 
 @author: javier
 '''
+from pyscipopt import Model, quicksum
 
 class Hiperplano(object):
     '''
@@ -16,3 +17,33 @@ class Hiperplano(object):
         '''
         self.__coefficients = q
         self.__intercept =  o
+    
+    def getDimension(self):
+        return len(self.__coefficients)
+    
+    def getCoefficient(self, index):
+        return self.__coefficients[index]
+    
+    def getIntercept(self):
+        return self.__intercept
+    
+    #TODO: Se debe verificar que el hiperplano pertenesca a la region dada
+    def isRedundant(self, region):
+        
+        dimension = self.getDimension()
+        ############ model ##############################
+        model = Model()
+        for i in range(dimension):
+            xVars = model.addVar(vtype="CONTINUOUS", name="x[%s]" %(i),lb=None)        
+        
+        
+        for hiperplane in region.getHiperplanes().difference(self):
+            model.addCons(quicksum(hiperplane.getCoefficient(f)*xVars(f) for f in range(dimension)) <= hiperplane.getIntercept(),"r1%s" % (hiperplane))
+        
+        model.addCons(quicksum(self.getCoefficient(f)*xVars[f] for f in range(dimension)) <= self.getIntercept() + 1,"prevent unbound")
+        
+        model.setObjective(quicksum(self.getCoefficient(f)*xVars[f] for f in range(dimension)), sense="maximize")  
+        model.optimize()
+        
+        return model.getObjVal() <= self.getIntercept()      
+
--- CRIOV.2/CRIO/Regionalization/__init__.py
+++ CRIOV.2/CRIO/Regionalization/__init__.py
@@ -41,7 +41,14 @@ def assingGroupsToRegions(group,cluster):
     
     ################################################################## 
 
-
+def isRedundant(region, clusters):
+    
+    def auxiliarModel(region, cluster):
+        pass
+    
+    
+    
+    
 
 
 
