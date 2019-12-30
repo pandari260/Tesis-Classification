d2fb944e389081d3eec7ac4feacf2492c7179167
Javier <xavierescobar3335@gmail.com>
2019-12-30 09:47:32 -0300

    Agregados test en 3 y 4 dimensiones para el cuarto modelo

--- CRIOV.2/TEST/HiperplaneTest.py
+++ CRIOV.2/TEST/HiperplaneTest.py
@@ -45,8 +45,82 @@ class HiperplaneTest(unittest.TestCase):
         
         
         self.assertFalse(h1.isRedundant(r),"el hiperplano x + y = 5.5 no es redundante")
-        self.assertFalse(h2.isRedundant(r), "el hiperplano x + y = 6 es redundante por el hiperplano x 'y = 5.5")
-        self.assertFalse(h3.isRedundant(r), "el hiperplano x + y = 7 es redundante por el hiperplano x 'y = 5.5")
+        self.assertFalse(h2.isRedundant(r), "el hiperplano 2x + y = 6 no es redundante")
+        self.assertFalse(h3.isRedundant(r), "el hiperplano 3x + y = 7 no es redundante")
+    
+    def test_isRedundat_onlyOneHyperplane_3d(self):
+        d = 3
+        h = Hyperplane([1.0,1.0,0.0],5)
+        r = Region([(2.0,1.0,0.0),(2.0,2.0,0.0),(3.0,1.0,0.0),(3.0,2.0,0.0)],[h],d)
+        
+        resul = h.isRedundant(r)
+        self.assertFalse(resul,"no puede ser redundante si no hay otro hiperplanos")
+        pass
+    
+    def test_isRedundat_thereIsRedundant_3d(self):
+        d = 3
+        h1 = Hyperplane([1.0,1.0,0.0],5.5)
+        h2 = Hyperplane([1.0,1.0,0.0],6.0)
+        h3 = Hyperplane([1.0,1.0,0.0],7.0)
+
+        r = Region([(2.0,1.0,0.0),(2.0,2.0,0.0),(3.0,1.0,0.0),(3.0,2.0,0.0)],[h1,h2,h3],d)
+        
+        
+        self.assertFalse(h1.isRedundant(r),"el hiperplano x + y = 5.5 no es redundante")
+        self.assertTrue(h2.isRedundant(r), "el hiperplano x + y = 6 es redundante por el hiperplano x 'y = 5.5")
+        self.assertTrue(h3.isRedundant(r), "el hiperplano x + y = 7 es redundante por el hiperplano x 'y = 5.5")
+
+        pass
+    
+    def test_isRedundant_thereIsNotRedundant_3d(self):
+        d = 3
+        h1 = Hyperplane([1.0,1.0,0.0],5.5)
+        h2 = Hyperplane([2.0,1.0,0.0],6.0)
+        h3 = Hyperplane([3.0,1.0,0.0],7.0)
+
+        r = Region([(0.0,1.0,0.0),(1.0,2.0,0.0),(1.0,1.0,0.0)],[h1,h2,h3],d)
+        
+        
+        self.assertFalse(h1.isRedundant(r),"el hiperplano x + y = 5.5 no es redundante")
+        self.assertFalse(h2.isRedundant(r), "el hiperplano 2x + y = 6 no es redundante")
+        self.assertFalse(h3.isRedundant(r), "el hiperplano 3x + y = 7 no es redundante")
+    
+    def test_isRedundat_onlyOneHyperplane_4d(self):
+        d = 4
+        h = Hyperplane([0.0,1.0,1.0,0.0],5)
+        r = Region([(0.0,2.0,1.0,0.0),(0.0,2.0,2.0,0.0),(0.0,3.0,1.0,0.0),(0.0,3.0,2.0,0.0)],[h],d)
+        
+        resul = h.isRedundant(r)
+        self.assertFalse(resul,"no puede ser redundante si no hay otro hiperplanos")
+        pass
+    
+    def test_isRedundat_thereIsRedundant_4d(self):
+        d = 4
+        h1 = Hyperplane([0.0,1.0,1.0,0.0],5.5)
+        h2 = Hyperplane([0.0,1.0,1.0,0.0],6.0)
+        h3 = Hyperplane([0.0,1.0,1.0,0.0],7.0)
+
+        r = Region([(0.0,2.0,1.0,0.0),(0.0,2.0,2.0,0.0),(0.0,3.0,1.0,0.0),(0.0,3.0,2.0,0.0)],[h1,h2,h3],d)
+        
+        
+        self.assertFalse(h1.isRedundant(r),"el hiperplano x + y = 5.5 no es redundante")
+        self.assertTrue(h2.isRedundant(r), "el hiperplano x + y = 6 es redundante por el hiperplano x 'y = 5.5")
+        self.assertTrue(h3.isRedundant(r), "el hiperplano x + y = 7 es redundante por el hiperplano x 'y = 5.5")
+
+        pass
+    
+    def test_isRedundant_thereIsNotRedundant_4d(self):
+        d = 4
+        h1 = Hyperplane([0.0,1.0,1.0,0.0],5.5)
+        h2 = Hyperplane([0.0,2.0,1.0,0.0],6.0)
+        h3 = Hyperplane([0.0,3.0,1.0,0.0],7.0)
+
+        r = Region([(0.0,1.0,0.0),(1.0,2.0,0.0),(1.0,1.0,0.0)],[h1,h2,h3],d)
+        
+        
+        self.assertFalse(h1.isRedundant(r),"el hiperplano x + y = 5.5 no es redundante")
+        self.assertFalse(h2.isRedundant(r), "el hiperplano 2x + y = 6 no es redundante")
+        self.assertFalse(h3.isRedundant(r), "el hiperplano 3x + y = 7 no es redundante")
         
     
 
