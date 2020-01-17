07b84ec0018bb4568d6f81c82745112a33baefd6
Ariel <arieldominguezmp@gmail.com>
2020-01-17 12:36:17 -0300

    Menu testreal

--- TEST/MainTestReal.py
+++ TEST/MainTestReal.py
@@ -5,26 +5,28 @@ import Plotter as plotter
 import Exporter as exporter
 import pandas as pd
 
-f= loadmat('../../C_Easy1_noise01.mat')
+def __runTest(file):
+    c0, c1, c_ret = [], [], []
+    f= loadmat("../../Simulator/"+file+".mat")
 
-arrTime = f['spike_times'][0][0][0]
-arrClass  = f['spike_class'][0][1][0]
-arrData = f['data'][0]
-c0,c1,c_ret = [], [], []
+    arrTime = f['spike_times'][0][0][0]
+    arrClass  = f['spike_class'][0][1][0]
+    arrData = f['data'][0]
 
-print(arrClass)
-print(arrTime)
-for i in range(0, len(arrTime)):
-    clss = c0 if(arrClass[i] == 0) else c1
-    clss.append((round(arrData[arrTime[i]],5), arrTime[i], arrClass[i]))
+    for i in range(0, len(arrTime)):
+        clss = c0 if(arrClass[i] == 0) else c1
+        clss.append((round(arrData[arrTime[i]],5), arrTime[i], arrClass[i]))
 
-name = "../INPUT/REAL/SVM/t1-Simulacion1.csv"
-print(len(c0))
-print(len(c1))
-c_ret.append(c0)
-c_ret.append(c1)
-print(c1)
-#print(np.concatenate((c0,c1), axis=0)
-exporter.exportCaseTest(c_ret, name)
+    name = "../INPUT/REAL/SVM/Test-"+ file +".csv"
+    c_ret.append(c0)
+    c_ret.append(c1)
+    exporter.exportCaseTest(c_ret, name)
+
+
+file = ["C_Easy1_noise01",
+        "C_Easy1_noise005"]
+
+for i in file:  __runTest(i)
+
 
 
