987f52e53756f43bcadd0d66cfed47af92949969
Ariel <arieldominguezmp@gmail.com>
2020-02-01 14:42:15 -0300

    Cambios graficador

--- SVM/Main.py
+++ SVM/Main.py
@@ -16,29 +16,25 @@ test = [        "/t1-ConjuntosDisjuntos",
                 "/t7-DiagonalIntercalada",
                 "/t8-DiagonalIntercaladaSolapada"   ]
 
+test_real = [   "/Test-C_Easy1_noise01",
+                "/Test-C_Easy1_noise02",
+                "/Test-C_Easy1_noise03",
+                "/Test-C_Easy1_noise04",
+                "/Test-C_Easy1_noise005",
+                "/Test-C_Easy2_noise01",
+                "/Test-C_Easy2_noise02",
+                "/Test-C_Difficult1_noise01",
+                "/Test-C_Test_LFPcorr_Easy2_noise015",
+                "/Test-C_Drift_Easy2_noise015"    ]
+
 directory = ["R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"]
+directory_real = ["R2-Real"]
 d=0
+
+test = test_real
+directory = directory_real
 
 for i in range(len(test)):
     dataSet, target = searcher.searchDatafile(directory[d], test[i])
-    resultClassify, dataTest = svm.classify('rbf', dataSet, target)
+    resultClassify, dataTest = svm.classify('linear', dataSet, target)
     Printer.showResult(str(i+1), directory[d], test[i], resultClassify, dataTest)
-
-
-
-
-
-'''
-
-#__readAllTest(test, directory,d)
-
-file = '../INPUT/REAL/SVM/Test-C_Easy1_noise01.csv'
-
-fdata = pd.read_csv(file)
-dataSet = fdata.drop('Class', axis=1)
-target = fdata['Class']
-resultClassify, dataTest = svm.classify('rbf', dataSet, target)
-__showInfo(1, "R2-Real", 5000, resultClassify, dataTest)
-
-print("hola mundo")
-'''
--- TEST/mainParaGraficar.py
+++ TEST/mainParaGraficar.py
@@ -1,14 +1,28 @@
 from Importer import readSamples, readHiperplanes
 from Plotter import graphDataSet
 import numpy as np
+import csv
 
-routeClass0 = "class0.dat"
-routeClass1 = "class1.dat"
+def readFile(name):
+	c0, c1 = [], []
+	with open(name, 'r') as csvfile:
+		set_data = csv.reader(csvfile, delimiter=',')
+		next(set_data)
+		aux_set_data = list(set_data).copy()
+		c0 = list(map(lambda k: tuple(map(lambda i: float(i), k)),  list(map(lambda j: j[:-1], list(filter(lambda l: int(l[-1]) == 0, aux_set_data))))))
+		c1 = list(map(lambda k: tuple(map(lambda i: float(i), k)), list(map(lambda j: j[:-1], list(filter(lambda l: int(l[-1]) == 1, aux_set_data))))))
+	return c0, c1
+
 routeSolution = "solution"
+dimension = 2
+d = "R2"
+name = "../Resources/"+d+"/t7-DiagonalIntercalada.csv"
 
 def main():
-    clase0 = readSamples(routeClass0,2)
-    clase1 = readSamples(routeClass1,2)
+    #clase0 = readSamples(routeClass0,2)
+    #clase1 = readSamples(routeClass1,2)
+    clase0, clase1 = readFile(name)
+
     solution = readHiperplanes(routeSolution,2)
 
     print("Clase0: " + str(clase0)  + "\n\n\n\n\n\n")
