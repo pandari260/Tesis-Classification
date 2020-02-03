632985471ea332aefc09d4432504ec7af105236e
Ariel <arieldominguezmp@gmail.com>
2020-02-03 18:13:25 -0300

    Prueba de Classifier

--- CRIOV.2/CRIO/Modelo/Classifier.py
+++ CRIOV.2/CRIO/Modelo/Classifier.py
@@ -8,6 +8,8 @@ from CRIO.Grouping import createGroups
 from CRIO.Regionalization import createRegions
 from CRIO.Modelo.SampleContainer import SampleContainer
 import CRIO.Importer as Importer
+import numpy as np
+import Sample
 
 class Classifier(object):
     '''
@@ -75,26 +77,27 @@ class Classifier(object):
    
 
 def main():
+    
     d = 2
     k = 1
-    #c0 = SampleContainer([(-0.1663, -0.208), (-1.4265, 1.2276), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)],d)
-    #c1 = SampleContainer([(5.8384, 0.1114), (6.1911, -0.2508), (6.8148, -0.6143), (5.8302, -1.8956), (7.2234, 1.8256)], d)
-    c0,c1 = Importer.readSamples("/home/javier/Documentos/Repositorios Git/Tesis-Classification/INPUT/SVM/R2/t1-ConjuntosDisjuntos.csv", d)
+    c0 = SampleContainer([(-0.1663, -0.208), (-1.4265, 1.2276), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)],d)
+    c1 = SampleContainer([(5.8384, 0.1114), (6.1911, -0.2508), (6.8148, -0.6143), (5.8302, -1.8956), (7.2234, 1.8256)], d)
+    #c0,c1 = Importer.readSamples("/home/pandari/Escritorio/Tesis-Classification/Resources/R2/t1-ConjuntosDisjuntos.csv", d)
     
+    sample = [(-0.1663, -0.208), (-1.4265, 1.2276), (6.1911, -0.2508), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
     
     t0 = "rojo"
     t1 = "azul"
     clasifier = Classifier(c0,c1,t0,t1,d,k)
     clasifier.train()
     
-   
-    
-    clasifier.export("/home/javier/Documents/LiClipse Workspace/Ploteo/TEST/solution", d)
-
-
+    for i in sample:
+        print(clasifier.classify(Sample.Sample(i)))
     
 
+    
 
+main()
     
     
     
