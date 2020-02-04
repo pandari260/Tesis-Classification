2d45657749394a06f055395a53c5f3654d6e8a88
Ariel <arieldominguezmp@gmail.com>
2020-02-04 15:58:56 -0300

    Se implemento las metricas de clase y matriz de confusion

--- CRIOV.2/CRIO/Modelo/Classifier.py
+++ CRIOV.2/CRIO/Modelo/Classifier.py
@@ -8,8 +8,11 @@ from CRIO.Grouping import createGroups
 from CRIO.Regionalization import createRegions
 from CRIO.Modelo.SampleContainer import SampleContainer
 import CRIO.Importer as Importer
-import numpy as np
 import Sample
+import MetricsClassifier as Metrics
+import numpy as np
+from operator import ge
+
 
 class Classifier(object):
     '''
@@ -71,10 +74,18 @@ class Classifier(object):
             f.write(THREE_ITEMS_FORMAT % tuple(data))
         f.close()
         
+    def __rowConfuseMatrix(self, clasifier, sample, color):
+        r1, r2 = 0, 0
+        for i in sample:  
+            if(clasifier.classify(Sample.Sample(i)).__eq__(color)):  r1+=1 
+            else: r2+=1
+        return r1, r2
         
-        
-
-   
+    def generateConfuseMatrix(self, clasifier, sampleC0, sampleC1, t0, t1):
+        TP, FP = self.__rowConfuseMatrix(clasifier, sampleC0, t0)
+        TN, FN = self.__rowConfuseMatrix(clasifier, sampleC1, t1)  
+        return float(TP), float(FP), float(TN), float(FN)
+            
 
 def main():
     
@@ -84,7 +95,7 @@ def main():
     c1 = SampleContainer([(5.8384, 0.1114), (6.8148, -0.6143), (5.8302, -1.8956), (7.2234, 1.8256)], d)
     #c0,c1 = Importer.readSamples("/home/pandari/Escritorio/Tesis-Classification/Resources/R2/t1-ConjuntosDisjuntos.csv", d)
     
-    sampleC0 = [(-0.1663, -0.208), (-1.4265, 1.2276), (6.1911, -0.2508), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
+    sampleC0 = [(-0.1663, -0.208), (-1.4265, 1.2276), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
     sampleC1 = [(-0.1663, -0.208), (-1.4265, 1.2276), (6.1911, -0.2508), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
     
     t0 = "rojo"
@@ -92,17 +103,17 @@ def main():
     clasifier = Classifier(c0,c1,t0,t1,d,k)
     clasifier.train()
     
-    TP, FP, TN, FN = 0, 0, 0, 0
+    TP, FP, TN, FN = clasifier.generateConfuseMatrix(clasifier, sampleC0, sampleC1, t0, t1)
+    metC0 = Metrics.MetricsClassifier(0, TP, FP, TN, FN)
+    metC1 = Metrics.MetricsClassifier(1, FN, TN, FP, TP)
     
-    for i in sampleC0:
-        if(clasifier.classify(Sample.Sample(i)).__eq__("rojo")):  TP+=1 
-        else: FP+=1
-    
-    for i in sampleC1:
-        if(clasifier.classify(Sample.Sample(i)).__eq__("azul")):  TN+=1 
-        else: FN+=1
-    
-    print("Valor de TP: %d \nValor de FP: %d \nValor de TN: %d \nValor de FN: %d" % (TP, FP, TN, FN))
+    print ("Titulo del testsss")
+    print ("\nAccuracy: {}\n".format(metC0.getAccuracy()))    
+    print ("Matrix Confuse:\n|{}, {}|\n|{}, {}|\n".format(int(TP), int(FP), int(TN), int(FN)))
+    print ("Report:\n\tClass\tPresicion\tRecall\t\tF1-Score\tSupport\n")
+    metC0.showMetrics()
+    metC1.showMetrics()
+
    
 main()
     
