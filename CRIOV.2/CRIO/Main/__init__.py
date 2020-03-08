ff49e492dee5add71d51963a5213fa5c140ebf32
Javier <xavierescobar3335@gmail.com>
2020-03-08 19:36:07 -0300

    Se arreglo el orden de los vaores de la matriz de confucion

--- CRIOV.2/CRIO/Main/__init__.py
+++ CRIOV.2/CRIO/Main/__init__.py
@@ -25,17 +25,7 @@ def main():
     d = 2
     k = 1
     
-    c0,c1 = Importer.readSample("/home/javier/Documentos/Repositorios Git/Tesis-Classification/Resources/R2/t1-ConjuntosDisjuntos.csv")
-    
-    '''
-    train0 = [(-0.1663, -0.208), (-1.4265, 1.2276), (6.8148, -0.6143), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
-    train1 = [(-0.1663, -0.208), (-1.4265, 1.2276), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
-    
-    test0 = [(-0.1663, -0.208), (-1.4265, 1.2276), (6.8148, -0.6143), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
-    test1 = [(-0.1663, -0.208), (-1.4265, 1.2276), (-0.7036, 1.0372), (0.2668, -1.6665), (0.2529, -1.9605)]
-    '''
-    
-    
+    c0,c1 = Importer.readSample("/home/javier/Documentos/Repositorios Git/Tesis-Classification/Resources/R2/t1-ConjuntosDisjuntos.csv")   
     split_samples_0 = map(lambda cluster: divideProportionally(cluster, 0.9),c0)
     split_samples_1 = map(lambda cluster: divideProportionally(cluster, 0.9),c1)  
     
@@ -44,10 +34,6 @@ def main():
     test0 = [item for sublist in map(lambda s: s[1], split_samples_0) for item in sublist] 
     train1 = [item for sublist in map(lambda s: s[0], split_samples_1) for item in sublist] 
     test1 = [item for sublist in map(lambda s: s[1], split_samples_1) for item in sublist] 
-    
-
-    
-    
     
     train0 = SampleContainer(train0,d)
     train1 = SampleContainer(train1,d)
@@ -60,8 +46,8 @@ def main():
     TP, FP, TN, FN = ConfuseMatrix.generateConfuseMatrix(clasifier, test0, test1, t0, t1)
    
 
-    metC0 = MetricsClassifier(0, TP, FP, TN, FN)
-    metC1 = MetricsClassifier(1, FN, TN, FP, TP)
+    metC0 = MetricsClassifier(0, TP, FP, FN, TN)
+    metC1 = MetricsClassifier(1, TN, FN, FP, TP)
     
     TITLE_CASETEST ="Titulo del testsss"
     ACCURACY = "\nAccuracy: {}\n"
@@ -70,7 +56,7 @@ def main():
             
     print (TITLE_CASETEST)
     print (ACCURACY.format(metC0.getAccuracy()))    
-    print (CONFUSE_MATRIX.format(int(TP), int(FP), int(TN), int(FN)))
+    print (CONFUSE_MATRIX.format(int(TP), int(FP), int(FN), int(TN)))
     print (HEADER_METRIC)
     print (metC0.showMetrics())
     print (metC1.showMetrics())
