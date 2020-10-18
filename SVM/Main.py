0c394fa585ef4a492dcadb07673594c63356f6dd
Pandari <pandari260@gmail.com>
2020-10-18 16:45:46 -0300

    New files to process R12-20

--- SVM/Main.py
+++ SVM/Main.py
@@ -27,8 +27,8 @@ test = [    [   "/t1-ConjuntosDisjuntos",
                 "/Test-C_Test_LFPcorr_Easy2_noise015",
                 "/Test-C_Drift_Easy2_noise015"    ]  ]
 
-directory = ["R2", "R2-Real", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"]
-d=2
+directory = ["R2", "R2-Real", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10","R12","R13","R14","R15","R16","R17","R18","R19", "R20"]
+d=10
 test = test[0]
 
 
